9a4bfe9c7d14eb9c63230bdc5418053a8b3a28ab
Liam Ansoumane Kadher Kande <liamkande@icloud.com>
2023-06-03 18:19:05 -0700

    added logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PokemonState {
+  name: string
+  sprites: { other: { dream_world: { front_default: string } } }
 }
 
-export default App;
+interface PokemonProps {
+  url: string
+}
+
+const Pokemon: React.FC<PokemonProps> = ({ url }) => {
+  const [pokemon, setPokemon] = useState<PokemonState | null>(null)
+
+  const getPokemon = async () => {
+    const response = await fetch(url)
+    const data: PokemonState = await response.json()
+    setPokemon(data)
+  }
+
+  useEffect(() => {
+    getPokemon()
+  }, [])
+
+  if (!pokemon) return null
+
+  return (
+    <li className="pokemon">
+      <img
+        alt={pokemon.name}
+        src={pokemon.sprites.other.dream_world.front_default}
+      />
+      <strong>{pokemon.name}</strong>
+    </li>
+  )
+}
+
+const PAGE_SIZE = 50
+
+interface PokeListState {
+  page_num: number
+  pokemon_list: { url: string }[] | null
+}
+
+const PokeList: React.FC = () => {
+  const [page_num, setPageNum] = useState(0)
+  const [pokemon_list, setPokemonList] =
+    useState<PokeListState["pokemon_list"]>(null)
+
+  const getPokemonList = async () => {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${PAGE_SIZE}&offset=${
+        page_num * PAGE_SIZE
+      }`
+    )
+    const data: { results: { url: string }[] } = await response.json()
+    setPokemonList(data.results)
+  }
+
+  useEffect(() => {
+    getPokemonList()
+  }, [page_num])
+
+  const handlePrevPage = () => {
+    setPageNum((prevPageNum) => prevPageNum - 1)
+  }
+
+  const handleNextPage = () => {
+    setPageNum((prevPageNum) => prevPageNum + 1)
+  }
+
+  return (
+    <div>
+      {page_num >= 1 && <button onClick={handlePrevPage}>Prev</button>}
+      <button onClick={handleNextPage}>Next</button>
+      <ul>
+        {pokemon_list?.map((pokemon) => (
+          <Pokemon key={pokemon.url} url={pokemon.url} />
+        ))}
+      </ul>
+    </div>
+  )
+}
+
+export default PokeList
